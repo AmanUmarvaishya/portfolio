b4f641ccf51554991fe2968962892d22b959aee8
AmanUmarvaishya <amanumarvaishya9653021436@gmail.com>
2025-06-22 06:08:47 -0800

    Update ProjectCard.jsx
    n

--- src/Components/Projects/ProjectCard.jsx
+++ src/Components/Projects/ProjectCard.jsx
@@ -10,9 +10,7 @@ const ProjectCard = ({ title, main ,image ,link }) => {
       <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4">
        
         <a href={link}>       Source Code</a>
-        <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
-   
-        </button>
+        
           
       </div>
     </div>
